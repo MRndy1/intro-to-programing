dfbc49fe05a245794817603acd036c940eaaf9f6
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2024-02-21 10:11:29 +1100

    scratch2

---
+++ week2/scratch1.1.py
@@ -0,0 +1,18 @@
+cyber = int(input("Enter uour cyber grade: "))
+python = int(input("Enter uour python grade: "))
+networking = int(input("Enter uour networking grade: "))
+
+avg = (cyber + python + networking) / 3
+
+if avg >= 90:
+    print(f"your grade is {avg} and its a higher distinction")
+elif avg >= 76 and avg <= 90:
+    print(f"your grade is {avg} and its a distinction")
+elif avg >= 61 and avg <= 75:
+    print(f"your grade is {avg} and its a credit")
+elif avg >= 50 and avg <= 60:
+    print(f"your grade is {avg} and its a pass")
+elif avg < 50:
+    print(f"your grade is {avg} and you're fail")
+else:
+    print("plese enter a valid grade")
---
+++ week2/scratch2.py
@@ -0,0 +1,7 @@
+print("""
+Determine if someone qualifies for a loan, they must meet two conditions:
+Must earn at least $30,000/year
+Must have been employed for at least two years
+Check first condition, and if it is true, check second condition
+""")
+
