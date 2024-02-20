578d81755061b51e1a49e09d75f424515c93815d
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2024-02-21 10:43:24 +1100

    pract1

--- week2/scratch1.1.py
+++ week2/scratch1.1.py
@@ -6,11 +6,11 @@ avg = (cyber + python + networking) / 3
 
 if avg >= 90:
     print(f"your grade is {avg} and its a higher distinction")
-elif avg >= 76 and avg <= 90:
+elif avg >= 75 and avg <= 90:
     print(f"your grade is {avg} and its a distinction")
-elif avg >= 61 and avg <= 75:
+elif avg >= 60 and avg < 75:
     print(f"your grade is {avg} and its a credit")
-elif avg >= 50 and avg <= 60:
+elif avg >= 50 and avg < 60:
     print(f"your grade is {avg} and its a pass")
 elif avg < 50:
     print(f"your grade is {avg} and you're fail")
