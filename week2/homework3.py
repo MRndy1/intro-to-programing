c67780d14a0f17c058abbaff47f3fa72ae107394
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2024-02-21 20:08:47 +1100

    homework3

--- week2/homework3.py
+++ week2/homework3.py
@@ -12,19 +12,22 @@ import math
 # • The number of hot dogs that will be left over
 # • The number of hot dog buns that will be left over
 
-people = int(input("How many people"))
-hotdog_each_person = int(input("How many hotdog do you wanna give per each person"))
+people = int(input("How many people will be attending to eat hotdog: \n"))
+hotdog_each_person = int(input("How many hotdog do you wanna give for each person: \n"))
 
 one_hotdog_package = 10
 one_bun_package = 8
 
 hotdog_needed = people * hotdog_each_person
-hotdog_package_needed = hotdog_needed / one_hotdog_package
-rounded_hotdog_package = math.ceil(hotdog_package_needed)
-left_over_hotdog = int((rounded_hotdog_package - hotdog_package_needed) * 10)
+hotdog_package_needed = math.ceil(hotdog_needed / one_hotdog_package)
+hotdog_leftover = (hotdog_package_needed * one_hotdog_package) - hotdog_needed
 
-bun_package_needed = hotdog_needed / one_bun_package
-rounded_bun_package = math.ceil(bun_package_needed)
-left_over_bun = int((rounded_bun_package - bun_package_needed) * 10)
+bun_package_needed = math.ceil(hotdog_needed / one_bun_package)
+bun_leftover = (bun_package_needed * one_bun_package) - hotdog_needed
 
-print(left_over_bun)
+print(f"the minimum number of packages of hotdogs required is {hotdog_package_needed} package")
+print(f"the minimum number of packages of hotdogs buns required is {bun_package_needed} package")
+print(f"the number of hotdogs that will be left over is {hotdog_leftover} hotdog")
+print(f"the number of hotdog buns that will be left over is {bun_leftover} buns ")
+
+
