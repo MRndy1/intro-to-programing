16802ab7f3b5739fe08cc51e384105d28968e7c2
MRndy1 <123948911+MRndy1@users.noreply.github.com>
2024-02-28 07:29:41 +1100

    loops

---
+++ self_study/for_loops.py
@@ -0,0 +1,13 @@
+# for item in range(15):
+#     print(item)
+
+items = ['apple', 'banana', 'grape', 'orange']
+# for index, item in enumerate(items):
+#     print(index, item)
+
+for item in items:
+    if item == "grape":
+        break
+    print(item)
+
+    
---
+++ self_study/loops.py
@@ -0,0 +1,7 @@
+
+count = 0
+
+while count <= 10:
+    print("this is number " + str(count))
+    count += 1
+print("after")
